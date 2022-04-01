c64357390c7f47200f62c324cdf08b58ba76d3e0
amitkumar <akp4you2008@gmail.com>
2022-04-01 11:03:31 +0530

    second

--- testexe/testexe/Form1.Designer.cs
+++ testexe/testexe/Form1.Designer.cs
@@ -38,9 +38,9 @@ namespace testexe
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(376, 85);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(73, 15);
+            this.label1.Size = new System.Drawing.Size(90, 15);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Test Exe First";
+            this.label1.Text = "Test Exe Second";
             // 
             // textBox1
             // 
